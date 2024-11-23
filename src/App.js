948c7e0a128b08e7625f5c99ad43b0088fc2afaf
jignesh-c30 <jc639597@gmail.com>
2024-11-23 12:18:06 +0530

    Changed basename to react router

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Services from "./components/pages/Services";
 function App() {
   return (
     <>
-      <Router basename="/sudhirsynthesis">
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
